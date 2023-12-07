6453e1d3db1f843cf0cdb7b5074edc291d2721c6
alicfritz <0000932549@senaimgaluno.com.br>
2023-12-07 18:50:22 -0300

    fazendo o passo a passo django

--- catalogo/SiteCatalogo/views.py
+++ catalogo/SiteCatalogo/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1>teste<h1>')
+
+def pagina(request):
+    return render(request,'index.html')
--- catalogo/setup/settings.py
+++ catalogo/setup/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for setup project.
 
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'siteCatalogo/templates')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'trilhas/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
