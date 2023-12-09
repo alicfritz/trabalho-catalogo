1aa37cd1f07a3a936a14c290d66605b30fd36205
alicfritz <0000932549@senaimgaluno.com.br>
2023-12-08 21:47:30 -0300

    começando tudo de novo

--- catalogo/SiteCatalogo/apps.py
+++ catalogo/SiteCatalogo/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class SitecatalogoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'SiteCatalogo'
+    name = 'siteCatalogo'
